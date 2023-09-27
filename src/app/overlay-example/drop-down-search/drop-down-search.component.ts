ebe4601f069f9534a3e907da9937200d944dfee0
thrkrdk <thrkardak@gmail.com>
2023-09-27 22:43:17 +0300

    ek ayarlar eklendi

--- src/app/overlay-example/drop-down-search/drop-down-search.component.ts
+++ src/app/overlay-example/drop-down-search/drop-down-search.component.ts
@@ -1,15 +1,34 @@
-import { FocusMonitor } from '@angular/cdk/a11y';
-import { CdkConnectedOverlay } from '@angular/cdk/overlay';
 import {
-  ChangeDetectionStrategy,
   Component,
-  ElementRef,
   OnInit,
+  ChangeDetectionStrategy,
   ViewChild,
+  ElementRef,
 } from '@angular/core';
+import { Observable, merge, iif, EMPTY } from 'rxjs';
+import {
+  map,
+  filter,
+  startWith,
+  switchMap,
+  delay,
+} from 'rxjs/operators';
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { MatInput } from '@angular/material/input';
-import { Observable, filter, map, merge } from 'rxjs';
-
+import {
+  CdkConnectedOverlay,
+  ConnectedPosition,
+  ScrollStrategyOptions,
+  ScrollStrategy,
+  OverlayRef,
+} from '@angular/cdk/overlay';
+import { FormControl } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle'; 
+export interface State {
+  flag: string;
+  name: string;
+  population: string;
+}
 @Component({
   selector: 'app-drop-down-search',
   templateUrl: './drop-down-search.component.html',
@@ -18,26 +37,142 @@ import { Observable, filter, map, merge } from 'rxjs';
 })
 export class DropDownSearchComponent implements OnInit {
   showPanel$: Observable<boolean>;
-  private isPanelVisible$: Observable<boolean>;
-  private isPanelHidden$: Observable<boolean>;
+
+  
+  states: State[] = [
+    {
+      name: 'Ankara',
+      population: '5.663M',
+      flag:
+        'https://upload.wikimedia.org/wikipedia/tr/a/a1/Ankara_Büyükşehir_Belediyesi_logosu.png',
+    },
+    {
+      name: 'İstanbul',
+      population: '15.46M', 
+      flag:
+        'https://upload.wikimedia.org/wikipedia/tr/2/24/Ibb_amblem.png',
+    },
+    {
+      name: 'Adana',
+      population: '1.769M',
+      flag:
+        'https://upload.wikimedia.org/wikipedia/tr/f/ff/Adana_Büyükşehir_Belediyesi_logo.png',
+    },
+    {
+      name: 'İzmir',
+      population: '4.367K',
+      flag:
+        'https://upload.wikimedia.org/wikipedia/tr/d/df/İzmir_Büyükşehir_Belediyespor_logo.png',
+    },
+  ];
+
+  
+  stateCtrl = new FormControl();
+  filteredStates$: Observable<State[]>;
+  isCaseSensitive: boolean = false;
+  positions: ConnectedPosition[] = [
+    {
+      originX: 'center',
+      originY: 'bottom',
+      overlayX: 'center',
+      overlayY: 'top',
+      offsetY: -21,
+    },
+    {
+      originX: 'center',
+      originY: 'top',
+      overlayX: 'center',
+      overlayY: 'bottom',
+      panelClass: 'no-enogh-space-at-bottom',
+    },
+  ];
+
+  scrollStrategy: ScrollStrategy;
+
   @ViewChild(MatInput, { read: ElementRef, static: true })
   private inputEl: ElementRef;
 
   @ViewChild(CdkConnectedOverlay, { static: true })
   private connectedOverlay: CdkConnectedOverlay;
 
-  constructor(private focusMonitor: FocusMonitor) {}
+  private isPanelVisible$: Observable<boolean>;
+  private isPanelHidden$: Observable<boolean>;
+  private isOverlayDetached$: Observable<void>;
+
+  constructor(
+    private focusMonitor: FocusMonitor,
+    private scrollStrategies: ScrollStrategyOptions
+  ) {}
+
   ngOnInit(): void {
-    this.isPanelHidden$ = this.connectedOverlay.backdropClick.pipe(
-      map(() => false)
-    ); // brackdrop tıklandıkça false dönecek şekilde ayarlıyoruz
+    this.scrollStrategy = new ConfirmScrollStrategy(this.inputEl);
 
     this.isPanelVisible$ = this.focusMonitor.monitor(this.inputEl).pipe(
       filter((focused) => !!focused),
-      map(() => true)
-    ); // map ile focus olduğunda hep true döncek şekilde sabitledik
-    // ikinci stream yapmadan overlay n back drop u olması için hmtl de property ekle
-
+      map(()=>true)
+    );
+    this.isOverlayDetached$ = this.isPanelVisible$.pipe(
+      delay(0),
+      switchMap(() =>
+        iif(
+          () => !!this.connectedOverlay.overlayRef,
+          this.connectedOverlay.overlayRef.detachments(),
+          EMPTY
+        )
+      )
+    );
+    this.isPanelHidden$ = merge(
+      this.isOverlayDetached$,
+      this.connectedOverlay.backdropClick
+    ).pipe(map(()=>false));
     this.showPanel$ = merge(this.isPanelHidden$, this.isPanelVisible$);
+
+    this.filteredStates$ = this.stateCtrl.valueChanges.pipe(
+      startWith(''),
+      map((state) => (state ? this._filterStates(state) : this.states.slice()))
+    );
   }
+
+  setCaseSensitive({ checked }: MatSlideToggleChange) {
+    this.isCaseSensitive = checked;
+  }
+
+  private _filterStates(value: string): State[] {
+    const filterValue = this.caseCheck(value);
+
+    return this.states.filter(
+      (state) => this.caseCheck(state.name).indexOf(filterValue) === 0
+    );
+  }
+
+  private caseCheck(value: string) {
+    return this.isCaseSensitive ? value : value.toLowerCase();
+  }
+}
+
+class ConfirmScrollStrategy implements ScrollStrategy {
+  _overlay: OverlayRef;
+
+  constructor(private inputRef: ElementRef) {}
+
+  attach(overlayRef: OverlayRef) {
+    this._overlay = overlayRef;
+  }
+
+  enable() {
+    document.addEventListener('scroll', this.scrollListener);
+  }
+
+  disable() {
+    document.removeEventListener('scroll', this.scrollListener);
+  }
+
+  private scrollListener = () => {
+    if (confirm('The overlay will be closed. Procced?')) {
+      this._overlay.detach();
+      this.inputRef.nativeElement.blur();
+      return;
+    }
+    this._overlay.updatePosition();
+  };
 }
